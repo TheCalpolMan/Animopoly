4b839bfb6a4e27c2d69fbd7d9e5590bd06eb0b08
TheCalpolMan <43935058+TheCalpolMan@users.noreply.github.com>
2021-03-02 14:50:30 +0000

    Board partially implemented

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -1,5 +1,22 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Board {
-    private int playerNum;
+    private ArrayList<Animal> Board = new ArrayList<Animal>();
+
+    public Board(){
+        for (int i = 0; i < 26; i++) {
+            Board.add(new Animal());
+        }
+    }
+
+    public Animal getSpace(int space){
+        return Board.get(space);
+    }
+
+
+    public void upgradeSpace(int space){
+        getSpace(space).setLevel(getSpace(space).getLevel() + 1);
+    }
 }

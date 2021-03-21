7b823b382529a4fcc60904685096bb864d0f1c0d
Miguel Trumpetman <miguel.wareing@yahoo.com>
2021-03-21 16:07:45 +0000

    lar lar chimilchimg whats this sticky stuff on me

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,45 +1,57 @@
 package com.company;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Player {
     private boolean missTurn;
     private int money=0;
-    private String playing_piece="a";
+    private char playing_piece;
     private int space=0;
     private Random random = new Random();
     private int dice_roll=0;
+    private String name;
 
-    public Player(){
+    public Player(char playing_piece, String name){
         missTurn = false;
         dice_roll=getSpace();
         setSpace(space);
+        this.playing_piece=playing_piece;
+        this.name=name;
 
+    }
+
+    public String getName() {
+        return name;
     }
 
     public char getPiece(){
-        return '*';
+        return playing_piece;
     }
 
     public int getSpace(){
-        boolean double_dice;
-        int dice1=(random.nextInt(6)+1);
-        int dice2=(random.nextInt(6)+1);
-        this.space=(dice1+dice2+space);
-        if (dice1==dice2)
-        {
-            double_dice=true;
-        }
-        return(this.space);
+       return space;
     }
 
     public void setSpace(int space)
     {
-
+        this.space=space;
     }
 
+    public boolean roll()
+    {
+        boolean double_dice=false;
+        int dice1=(random.nextInt(6)+1);
+        int dice2=(random.nextInt(6)+1);
+        space=dice1+dice2+space;
+        if (dice1==dice2)
+        {
+            double_dice=true;
+        }
+        return(double_dice);
+    }
     public void deltaMoney(int money)
     {
-        this.money=money;
+        this.money+=money;
     }
 
     public boolean getMissTurn() {

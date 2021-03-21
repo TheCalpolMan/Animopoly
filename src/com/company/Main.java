82a7028d1698167e4f98c8a28fb04ff123e03d52
Miguel Trumpetman <miguel.wareing@yahoo.com>
2021-03-21 16:08:16 +0000

    lar lar chimilchimg whats this sticky stuff on me

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,13 @@
-package com.company;
-
-import java.util.ArrayList;
-
-public class Main {
-    public static void main(String[] args) {
-        Board board = new Board();
-        ArrayList<Player> players = new ArrayList<>();
-        players.add(new Player());
-
-        board.printBoard(players);
-    }
-}
+//package com.company;
+//
+//import java.util.ArrayList;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Board board = new Board();
+//        ArrayList<Player> players = new ArrayList<>();
+//        players.add(new Player());
+//
+//        board.printBoard(players);
+//    }
+//}
